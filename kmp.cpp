69b917bfc3d31be052690cae9857e62ae025705c
Ubuntu16.04_kepler <870131615@qq.com>
2016-11-26 23:39:53 +0800

    mm

---
+++ kmp.cpp
@@ -0,0 +1,9 @@
+/***********************************************************
+* Author       : M_Kepler
+* EMail        : hellohuangjinjie@gmail.com
+* Last modified: 2016-07-28 12:57:51
+* Filename     : kmp.cpp
+* Description  :
+**********************************************************/
+
+#include <iostream>
